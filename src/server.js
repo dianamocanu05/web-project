2037dcbb79ece88c04bbcfafad6a65d7134ed3b2
dianamocanu05 <diana.paula.mocanu@gmail.com>
2021-07-17 22:04:11 +0300

    first configs

---
+++ src/server.js
@@ -0,0 +1,7 @@
+// import express from "express";
+const express = require("express");
+const app = express();
+const PORT = 4000;
+
+app.get("/", (req,res) => res.send("Hello!"));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
